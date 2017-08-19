e8fdf9bd553eeb0e1e7cc535cd9b971bdae6d77d
Dima Spivak <dima@streamsets.com>
2017-08-18 21:39:33 -0700

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -28,4 +28,12 @@ setup(
     zip_safe=False,
     entry_points={'console_scripts': ['name-my-sprint = namemysprint:main']},
     include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
 )
